6adb1c7755adb8af3a6224489627f6c31d369038
arzynik <github@arzynik.com>
2014-07-23 17:00:09 -0700

    framework tests n shit

--- tests/ConfigTest.php
+++ tests/ConfigTest.php
@@ -16,58 +16,37 @@ class ConfigTest extends Tipsy_Test {
 	public function setUp() {
 		$this->tip = new Tipsy\Tipsy;
 		$this->useOb = true; // for debug use
-		
-		//$this->tip->config('../config.ini');
-		
-		
-/*
-$this->tip->config([
-	'db' => [
-		'host' => 'blah'
-	]
-], true);
-*/
-
-		
-		/*
-
-		
-		$this->tip->model('DBO/TestModel', function() {
-			$model = [
-				'testmodel' => function() {
-					die('testing');
-				}
-			];
-			return $model;
-		});
-		
-		$this->tip->model('DBO/FileModel', function() {
-			$model = [
-				'filemodel' => function() {
-					die('testing');
-				},
-
-//				'construct' => function() {
-//					$this->_id_var = 'id';
-//					$this->_table = 'file';
-//				},
-
-				'id' => 'id',
-				'table' => 'upload'
-			];
-
-		
-			return $model;
-		});
-		*/
-		
-		
-
-
 	}
 
-	public function testRouterBasic() {
+	public function testConfigInternal() {
 		$_REQUEST['__url'] = 'router/basic';
+		
+		$this->tip->config([
+			'db' => [
+				'host' => 'blah'
+			]
+		], true);
+		
+		$this->ob();
+
+		$this->tip->router()
+			->when('router/basic', function() {
+				echo 'YES';
+			});
+		$this->tip->start();
+		
+		$check = $this->ob(false);
+		
+		$this->assertTrue($check == 'YES');
+	}
+	
+	public function testConfigFile() {
+		$_REQUEST['__url'] = 'router/basic';
+		
+		$this->tip->config('config.ini');
+		print_r($this->tip->config());
+		exit;
+
 		
 		$this->ob();
 
