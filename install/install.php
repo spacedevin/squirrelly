809c8ba30d1a5d2a92f304c6362d8a4fad459857
Devin Smith <github@arzynik.com>
2015-11-27 13:15:35 -0800

    fixed install params

--- install/install.php
+++ install/install.php
@@ -3,7 +3,7 @@
 
 // create the database
 $url = parse_url(getenv('CLEARDB_DATABASE_URL') ? getenv('CLEARDB_DATABASE_URL') : getenv('DATABASE_URL'));
-$sql = file_get_contents('install/db.'.$url['scheme'].'sql');
+$sql = file_get_contents('install/db.'.($url['scheme'] == 'postgressql' ? 'pgsql' : 'mysql').'sql');
 
 $db = new \PDO($url['scheme'].':host='.$url['host'].($url['port'] ? ';port='.$url['port'] : '').';dbname='.substr($url['path'], 1).';charset=utf8', $url['user'], $url['pass'], $options);
 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
