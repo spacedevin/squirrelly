13247f3d8c68c6819f0fd58f97c43bc8f3df1228
Devin Smith <github@arzynik.com>
2015-11-27 14:12:20 -0800

    fixed install path

--- install/install.php
+++ install/install.php
@@ -4,7 +4,7 @@
 // create the database
 $url = parse_url(getenv('CLEARDB_DATABASE_URL') ? getenv('CLEARDB_DATABASE_URL') : getenv('DATABASE_URL'));
 $type = $url['scheme'] == 'postgres' ? 'pgsql' : 'mysql';
-$sql = file_get_contents('install/db.'.$type.'.sql');
+$sql = file_get_contents('install/'.$type.'.sql');
 
 $db = new \PDO($type.':host='.$url['host'].($url['port'] ? ';port='.$url['port'] : '').';dbname='.substr($url['path'], 1).';charset=utf8', $url['user'], $url['pass'], $options);
 $db->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
